0b212d88f29a899b2aaa35a6bcffbcf242381bfd
Stig B. Sivertsen <sbsivertsen@gmail.com>
2020-01-18 15:49:13 +0100

    Learn to send BME 280 sensor data using 433MHz ASK Wireless Module kit using Arduino Nano, Did code cleanup and code formatting

--- BaseStation/BaseStation/BaseStation.ino
+++ BaseStation/BaseStation/BaseStation.ino
@@ -1,27 +1,68 @@
+/*
+ * Written by Stig B. Sivertsen
+ * sbsivertsen@gmail.com
+ * https://github.com/datamann/GA-weather-station
+ * 18.01.2020
+ * @see The GNU Public License (GPL) Version 3
+*/
 #include <RH_ASK.h>
 #include <SPI.h>
 
-//RH_ASK driver;
-RH_ASK driver(2000, 2, 4, 5);
+RH_ASK rf(2000, 2, 4, 5);
+
+#define DEBUG
+
+struct wd {
+    float temperature;
+    float altitude;
+    float pressure;
+    float humidity;
+    //float battery;      // For future use
+    //uint8_t  sensorID;  // For future use
+    //time_t timeStamp;   // For future use
+};
+wd weatherdata;
 
 void setup() {
   
-  Serial.begin(9600);  // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
-
+    Serial.begin(9600);
+    
+    if (!rf.init())
+    {
+        #ifdef DEBUG
+            Serial.println(F("RF init failed!"));
+        #endif
+    }
 }
 
 void loop() {
 
-  uint8_t buf[12];
-    uint8_t buflen = sizeof(buf);
-    if (driver.recv(buf, &buflen)) // Non-blocking
+    //uint8_t buf[];
+    //uint8_t buflen = sizeof(buf);
+
+    uint8_t data[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(data);
+    
+    if (rf.recv(data, &len)) // Non-blocking
     {
-      int i;
-      // Message with a good checksum received, dump it.
-      Serial.print("Message: ");
-      Serial.println((char*)buf);         
-    }
+        wd* weatherdata = (wd*)data;
+
+        #ifdef DEBUG
+            char buf[21];
   
+            char temperature[6];
+            char pressure[5];
+            char altitude[6];
+            char humidity[5];
+      
+            dtostrf(weatherdata->temperature, 3, 2, temperature);
+            dtostrf(weatherdata->pressure, 3, 0, pressure);
+            dtostrf(weatherdata->altitude, 3, 2, altitude);
+            dtostrf(weatherdata->humidity, 3, 0, humidity);
+      
+            sprintf(buf,"%s %s %s %s", temperature, pressure, altitude, humidity);
+            Serial.print(F("Buf: "));
+            Serial.println(buf);
+        #endif
+    }
 }
